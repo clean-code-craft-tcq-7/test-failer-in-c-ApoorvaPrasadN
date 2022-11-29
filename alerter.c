5be4a48c2d312f6dc6033bcebb51a747ba142f59
ApoorvaPrasadN <105204469+ApoorvaPrasadN@users.noreply.github.com>
2022-11-29 21:17:28 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <assert.h>
-
+#define SW_TEST 0
 int alertFailureCount = 0;
-
+#if(SW_TEST == 1)
 int networkAlertStub(float celcius) {
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
     // Return 200 for ok
@@ -10,10 +10,13 @@ int networkAlertStub(float celcius) {
     // stub always succeeds and returns 200
     return 200;
 }
+#endif
 
 void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
+    #if(SW_TEST == 1)
     int returnCode = networkAlertStub(celcius);
+    #endif
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
