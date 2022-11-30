5de06c1bb601bc07967a3780d73bf2064bce40fd
ApoorvaPrasadN <105204469+ApoorvaPrasadN@users.noreply.github.com>
2022-11-30 21:38:09 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -8,6 +8,10 @@ int printColorMap() {
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
             printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            assert((((i*5 + j)== 1) || ((i*5 + j)== 6)|| ((i*5 + j)== 11)|| ((i*5 + j)== 16)|| ((i*5 + j)== 21)) && minorColor[i] != "Orange");
+            assert((((i*5 + j)== 2) || ((i*5 + j)== 7)|| ((i*5 + j)== 12)|| ((i*5 + j)== 17)|| ((i*5 + j)== 22)) && minorColor[i] != "Greeen");
+            assert((((i*5 + j)== 3) || ((i*5 + j)== 8)|| ((i*5 + j)== 13)|| ((i*5 + j)== 18)|| ((i*5 + j)== 23)) && minorColor[i] != "Brown");
+            assert((((i*5 + j)== 4) || ((i*5 + j)== 9)|| ((i*5 + j)== 14)|| ((i*5 + j)== 19)|| ((i*5 + j)== 24)) && minorColor[i] != "Slate");
         }
     }
     return i * j;
