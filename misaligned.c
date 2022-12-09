90e73e678d00be7fe4b3e8818b27a94ff9c6f49a
ApoorvaPrasadN <105204469+ApoorvaPrasadN@users.noreply.github.com>
2022-12-09 13:30:04 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,24 +1,51 @@
 #include <stdio.h>
 #include <assert.h>
+    
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"}; 
 
+void getformat_Colormap();
+const int total_color = 25;
+struct colorMap
+{
+    int pairNumber;
+    char *majorColor;
+    char *minorColor;
+};
+struct colorMap formatted_ColorMap[total_color];
 int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
-            assert((((i*5 + j)== 1) || ((i*5 + j)== 6)|| ((i*5 + j)== 11)|| ((i*5 + j)== 16)|| ((i*5 + j)== 21)) && (minorColor[i] != minorColor[1]));
-            assert((((i*5 + j)== 2) || ((i*5 + j)== 7)|| ((i*5 + j)== 12)|| ((i*5 + j)== 17)|| ((i*5 + j)== 22)) && (minorColor[i] != minorColor[2]));
-            assert((((i*5 + j)== 3) || ((i*5 + j)== 8)|| ((i*5 + j)== 13)|| ((i*5 + j)== 18)|| ((i*5 + j)== 23)) && (minorColor[i] != minorColor[3]));
-            assert((((i*5 + j)== 4) || ((i*5 + j)== 9)|| ((i*5 + j)== 14)|| ((i*5 + j)== 19)|| ((i*5 + j)== 24)) && (minorColor[i] != minorColor[4]));
+            printf("%d | %s | %s\n", i * 5 + j, formatted_ColorMap[i*5+j].majorColor, formatted_ColorMap[i*5+j].minorColor);
         }
     }
     return i * j;
 }
 
+void getformat_Colormap()
+{
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            
+           formatted_ColorMap[i*5+j].pairNumber = i * 5 + j;
+           formatted_ColorMap[i*5+j].majorColor = (char *)majorColor[i];
+           formatted_ColorMap[i*5+j].minorColor = (char *)minorColor[i];
+        }
+    }
+          
+}
+
 int main() {
-    int result = printColorMap();
+    getformat_Colormap();
+    printColorMap();
+    for(int i =0; i<=total_color;i++)
+    {
+    assert(formatted_ColorMap[i].minorColor == minorColor[i]);
+    }
     assert(result == 25);
     printf("All is well (maybe!)\n");
     return 0;
